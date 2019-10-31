891249961b5990660dc9cda48c39acea3e34c2f5
augustovictor <victoraweb@gmail.com>
2019-10-31 19:55:17 -0300

    Initial commit

---
+++ src/main/kotlin/com/augustovictor/medavailable/MedAvailableApplication.kt
@@ -0,0 +1,11 @@
+package com.augustovictor.medavailable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedAvailableApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedAvailableApplication>(*args)
+}
---
+++ src/test/kotlin/com/augustovictor/medavailable/MedAvailableApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.augustovictor.medavailable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedAvailableApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
